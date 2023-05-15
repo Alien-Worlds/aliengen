88dac82e91c58888ccce376b514414622042b4a1
Ahmad Tirmazi <16874651+ahmadtirmazi@users.noreply.github.com>
2023-05-15 17:58:45 +0200

    refactor(abi generate): generate action enums

---
+++ src/actions/abi/generate/actions/enums.actions.ts
@@ -0,0 +1,36 @@
+import { Abi, Action } from "../../types/abi.types";
+
+import { GeneratedOutput } from "../generate.types";
+import TemplateEngine from "../template-engine";
+import { enumsTemplate } from "../templates";
+import { paramCase } from "change-case";
+import path from "path";
+
+export const generateActionEnums = (
+    abi: Abi,
+    contract: string,
+    baseDir: string,
+): GeneratedOutput[] => {
+    const actions: string[] = abi.actions.map((action: Action) => action.name)
+
+    const enumsContent = TemplateEngine.GenerateTemplateOutput(enumsTemplate, {
+        contract,
+        actions,
+    });
+
+    return createOutput(contract, enumsContent, baseDir);
+};
+
+const createOutput = (contract: string, enumsOutput: string, outputBaseDir: string): GeneratedOutput[] => {
+    const output: GeneratedOutput[] = [];
+
+    // write to file e.g. src/contracts/index-worlds/actions/domain/enums.ts
+    const outDir = path.parse(`${outputBaseDir}/contracts/${paramCase(contract)}/actions/domain/enums.ts`);
+
+    output.push({
+        filePath: path.format(outDir),
+        content: enumsOutput,
+    })
+
+    return output;
+};
--- src/actions/abi/generate/templates/index.ts
+++ src/actions/abi/generate/templates/index.ts
@@ -1,3 +1,4 @@
 export const dtosTemplate = 'dtos.hbs';
 export const exportsTemplate = 'exports.hbs';
 export const entityTemplate = 'entity.hbs';
+export const enumsTemplate = 'enums.hbs';
