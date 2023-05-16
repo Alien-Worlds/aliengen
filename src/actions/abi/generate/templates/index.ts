3f22a419dff64a9e4b4ec2db6a786a703cbf9de9
Ahmad Tirmazi <16874651+ahmadtirmazi@users.noreply.github.com>
2023-05-16 14:10:30 +0200

    refactor(abi generate): generate collective action entity

--- src/actions/abi/generate/templates/index.ts
+++ src/actions/abi/generate/templates/index.ts
@@ -2,6 +2,7 @@ export const dtosTemplate = 'dtos.hbs';
 export const collectiveDataTypeTemplate = 'collective-dto.hbs';
 export const exportsTemplate = 'exports.hbs';
 export const entityTemplate = 'entity.hbs';
+export const collectiveEntityTemplate = 'collective-entity.hbs';
 export const enumsTemplate = 'enums.hbs';
 export const dataSourceTemplate = 'data-source.hbs';
 export const repositoryTemplate = 'repository.hbs';
